43b1a9ccec33a72712b999332cd7a23130acec09
zhuchi76 <tzuchichen.sc08@nycu.edu.tw>
2023-12-24 21:24:25 +0800

    mod install

--- install.sh
+++ install.sh
@@ -4,24 +4,14 @@
 
 # Script to install Python 3, pip3, and required Python packages
 
-# Update the package list
-echo "Updating package list..."
-sudo apt-get update
-
-# Install Python 3
-echo "Installing Python 3..."
-sudo apt-get install -y python3
-
-# Check if pip3 is installed, install if it's not
-if ! command -v pip3 &> /dev/null
-then
-    echo "pip3 not found, installing pip3..."
-    sudo apt-get install -y python3-pip
-else
-    echo "pip3 is already installed."
-fi
-
-sudo apt-get install -y python3-opencv==4.6.0
+# Install build dependencies
+sudo apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    python3 \
+    python3-dev \
+    python3-pip \
+    g++
 
 # Install packages from requirements.txt
 echo "Installing Python packages from requirements.txt..."
