32d492fc33ac9c42d1ed7fb1a68152dfb592f39d
zhuchi76 <tzuchichen.sc08@nycu.edu.tw>
2023-12-24 21:42:13 +0800

    mod Dockerfile

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y \
+sudo apt-get update && apt-get install -y \
     build-essential \
     cmake \
-    python \
-    python-dev \
-    python-pip \
+    python3 \
+    python3-dev \
+    python3-pip \
     g++
 
 # Install packages from requirements.txt
 echo "Installing Python packages from requirements.txt..."
-sudo pip install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
 echo "Installation complete."
