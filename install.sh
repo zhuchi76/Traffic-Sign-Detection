11159cc33260030c1e31b6d031c7ccf94995179d
zhuchi76 <tzuchichen.sc08@nycu.edu.tw>
2023-12-24 21:29:57 +0800

    mod install

--- install.sh
+++ install.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# install.sh
-
-# Script to install Python 3, pip3, and required Python packages
-
-# Install build dependencies
-sudo apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
     build-essential \
     cmake \
-    python3 \
-    python3-dev \
-    python3-pip \
+    python \
+    python-dev \
+    python-pip \
     g++
 
 # Install packages from requirements.txt
 echo "Installing Python packages from requirements.txt..."
-pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 
 echo "Installation complete."
