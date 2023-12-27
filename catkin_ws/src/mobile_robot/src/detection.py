01cf5a408737a3d9a95fe39d50ab7db270044a45
zhuchi76 <tzuchichen.sc08@nycu.edu.tw>
2023-12-27 16:13:19 +0800

    mod detection.py traffic_sign_detector.launch

--- catkin_ws/src/mobile_robot/src/detection.py
+++ catkin_ws/src/mobile_robot/src/detection.py
@@ -2,6 +2,8 @@
 
 import rospy
 from std_msgs.msg import Bool, String
+from sensor_msgs.msg import Image  # Import the Image message type
+from cv_bridge import CvBridge, CvBridgeError  # Import CvBridge
 import cv2
 from utils.classification import SVM, getLabel
 from utils.utils import *
@@ -9,46 +11,38 @@ import os
 
 # Initialize global variables
 start_detection = False
-sign_counts = {}
-
-def load_model(model_path):
-    svm_model = SVM()
-    svm_model.load(model_path)
-    return svm_model
+current_image = None  # This will hold the latest image received from the camera
+bridge = CvBridge()  # Instantiate a CvBridge object
 
 def callback_start_detection(data):
     global start_detection
     start_detection = data.data
 
-def detect_signs(model, display_results):
-    
-    for i in range(1, 11):
-        image = cv2.imread(os.path.join("images", f"{i}.jpg"))
-        if image is not None:
-            coordinate, sign_type, text = localization(image, 300, 0.65, model)
-            
-            # Optionally display the results based on the ROS argument
-            if display_results:
-                processed_image = draw_picture(image, coordinate, sign_type, text)
-                cv2.imwrite(f'detection_{i}.png', processed_image)
-                cv2.imshow(f'detection_{i}.png', processed_image)
+def callback_image(data):
+    global current_image
+    try:
+        # Convert the ROS Image message to an OpenCV image
+        current_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+        rospy.logerr(e)
 
-            sign_counts[text] = sign_counts.get(text, 0) + 1
+def detect_signs(model, display_results, image):
+    if image is None:
+        return "No Image"
 
-    # Find the maximum count
-    max_count = max(sign_counts.values(), default=0)
-
-    # Find the first key with this maximum count
-    for key, value in sign_counts.items():
-        if value == max_count:
-            return key
+    coordinate, sign_type, text = localization(image, 300, 0.65, model)
+    # Optionally display the results based on the ROS argument
+    if display_results:
+        processed_image = draw_picture(image, coordinate, sign_type, text)
+        cv2.imwrite('detection.png', processed_image)
+        cv2.imshow('Detection', processed_image)
+        # cv2.waitKey(1)
+    return text
 
 def main():
     global model, pub, sub
 
     # Load the trained SVM model
-    # model = load_model("data_svm.dat")
-    # model = training()
     model = cv2.ml.SVM_load(os.path.join("utils", "data_svm.dat"))
 
     # Initialize ROS node
@@ -58,8 +52,8 @@ def main():
     pub = rospy.Publisher('traffic_sign', String, queue_size=10)
     sub = rospy.Subscriber('start_detection', Bool, callback_start_detection)
 
-    # ROS Subscriber 
-    sub = rospy.Subscriber('image', Bool, callback_image)
+    # ROS Subscriber for RPi Camera
+    sub_from_rpi_cam = rospy.Subscriber('image', Image, callback_image)
 
     # Set the rate of the loop
     rate = rospy.Rate(10)  # 10hz
@@ -69,8 +63,8 @@ def main():
     display_results = rospy.get_param("~display", False)
 
     while not rospy.is_shutdown():
-        if start_detection:
-            most_common_sign = detect_signs(model, display_results)
+        if start_detection and current_image is not None:
+            most_common_sign = detect_signs(model, display_results, current_image)
             pub.publish(most_common_sign)
             rospy.loginfo(f'Published Most Common Traffic Sign: {most_common_sign}')
         else:
